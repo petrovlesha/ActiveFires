2e1d4ab0ed7153c15f2841be8a3e1d8a82becef4
Stayn <nastyaloginovaa@gmail.com>
2016-05-29 11:34:48 +0300

    Added spatial clustering

--- src/main/java/app/WorldWindApplication.java
+++ src/main/java/app/WorldWindApplication.java
@@ -3,30 +3,15 @@ package app;
 import data.DataCreator;
 import gov.nasa.worldwind.BasicModel;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
-import gov.nasa.worldwind.formats.shapefile.Shapefile;
-import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.layers.Layer;
 import gov.nasa.worldwind.layers.RenderableLayer;
-import gov.nasa.worldwind.render.PointPlacemark;
 import gov.nasa.worldwind.util.StatusBar;
-import gov.nasa.worldwindx.examples.Placemarks;
-import org.apache.commons.io.FileUtils;
-import org.postgis.PGgeometry;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import static gov.nasa.worldwind.geom.Position.fromDegrees;
 
 /**
  * @author Nastya Loginova
@@ -40,26 +25,27 @@ public class WorldWindApplication {
     JRadioButton oneday;
     JRadioButton twodays;
     JRadioButton sevendays;
+    JRadioButton countries;
+    JRadioButton points;
     JButton refresh;
     StatusBar statusBar;
 
 
     public static void main(String arguments[]) {
-//        DataCreator.getCountries();
-//        DataCreator.isUpToDate("modis",1);
         new WorldWindApplication().launch();
     }
 
     public void launch() {
-        frame = new JFrame("Active Fire Data Visualization");
+        frame = new JFrame("Active Fires");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         JPanel globe = new JPanel(new BorderLayout(10, 10));
-        JPanel panel = new JPanel(new GridLayout(3, 1));
+        JPanel panel = new JPanel(new GridLayout(4, 1));
         JPanel panelSatellites = new JPanel(new GridLayout(2, 1));
         JPanel panelTime = new JPanel(new GridLayout(3, 1));
+        JPanel panelView = new JPanel(new GridLayout(2, 1));
         JPanel panelActions = new JPanel(new GridLayout(1, 1));
 
         wwd = new WorldWindowGLCanvas();
@@ -88,6 +74,16 @@ public class WorldWindApplication {
         panelTime.add(twodays);
         panelTime.add(sevendays);
 
+        countries = new JRadioButton("Countries", false);
+        points = new JRadioButton("Points", true);
+        Border borderView = BorderFactory.createTitledBorder("View");
+        panelView.setBorder(borderView);
+        ButtonGroup views = new ButtonGroup();
+        views.add(countries);
+        views.add(points);
+        panelView.add(countries);
+        panelView.add(points);
+
         refresh = new JButton("Refresh");
         ButtonGroup actions = new ButtonGroup();
         actions.add(refresh);
@@ -95,6 +91,7 @@ public class WorldWindApplication {
 
         panel.add(panelSatellites);
         panel.add(panelTime);
+        panel.add(panelView);
         panel.add(panelActions);
 
         refresh.addActionListener(new ActionListener() {
@@ -114,79 +111,29 @@ public class WorldWindApplication {
         frame.pack();
         frame.setVisible(true);
 
-        if (wwd.getModel().getLayers().getLayerByName("countries") == null) {
-            ShapefileLoader loader = new ShapefileLoader();
-            Shapefile countries = new Shapefile(new File("TM_WORLD_BORDERS_SIMPL-0.3/TM_WORLD_BORDERS_SIMPL-0.3.shp"));
-            Layer c = loader.createLayerFromShapefile(countries);
-            c.setName("countries");
-            c.setOpacity(0.1);
-
-            wwd.getModel().getLayers().add(c);
-        }
     }
 
-    public void update(){
+    public void update() {
         String satname = "modis";
         if (viirs.isSelected())
             satname = "viirs";
         int days = 1;
         if (twodays.isSelected())
             days = 2;
+        else if (sevendays.isSelected())
+            days = 7;
+        DataCreator.downloadAndUpdate(satname, days);
+        RenderableLayer l;
+        if (countries.isSelected())
+            l = DataCreator.getFireCountries(satname);
         else
-            if (sevendays.isSelected())
-                days=7;
-        DataCreator.downloadAndUpdate(satname,days);
-
-        File shpfile = new File("m.shp");
-
-        String cmd = "pgsql2shp -f "+shpfile.getAbsolutePath()+" -h localhost " +
-                "-u postgres -P postgres postgres \"select * from public."+satname+"fire\"";
-
-        ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", cmd);
-        try {
-            Process p = pb.start();
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(p.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(p.getErrorStream()));
-            String s;
-            // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
-            if (p.waitFor() != 0)
-                throw new Exception("Database read problem");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        ShapefileLoader loader = new ShapefileLoader();
-        Shapefile shp = new Shapefile(shpfile);
-        Layer l = loader.createLayerFromShapefile(shp);
-        l.setName("mylayer");
-        l.setOpacity(0.2);
-        Layer last = wwd.getModel().getLayers().getLayerByName("mylayer");
+            l = DataCreator.getPointsLayer(satname);
+        Layer last = wwd.getModel().getLayers().getLayerByName("countries");
+        if (last != null)
+            wwd.getModel().getLayers().remove(last);
+        last = wwd.getModel().getLayers().getLayerByName("points");
         if (last != null)
             wwd.getModel().getLayers().remove(last);
         wwd.getModel().getLayers().add(l);
-
-
-//        try {
-//            FileUtils.deleteDirectory(shpfile.getParentFile());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
     }
-
-
 }
--- src/main/java/data/DataCreator.java
+++ src/main/java/data/DataCreator.java
@@ -1,8 +1,17 @@
 package data;
 
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.*;
 import org.apache.commons.io.FileUtils;
+import org.postgis.Geometry;
 import org.postgis.PGgeometry;
+import org.postgis.Point;
+import org.postgis.Polygon;
 
+import java.awt.*;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
@@ -10,8 +19,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
+import org.postgis.Polygon;
 
 public class DataCreator {
     public static void downloadFileFromURL(String address, String localFileName) {
@@ -79,8 +91,9 @@ public class DataCreator {
 
             zis.closeEntry();
             zis.close();
-            PrintWriter out = new PrintWriter(outputFolder + File.separator + fileName.replaceFirst("\\.[a-zA-Z]+",".prj"));
-            out.print("GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.0,298.257223563]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]]");
+            PrintWriter out = new PrintWriter(outputFolder + File.separator + fileName.replaceFirst("\\.[a-zA-Z]+", ".prj"));
+            out.print("GEOGCS[\"GCS_WGS_1984\",DATUM[\"D_WGS_1984\",SPHEROID[\"WGS_1984\",6378137.0,298.257223563]]," +
+                    "PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]]");
             out.close();
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -91,7 +104,7 @@ public class DataCreator {
 //        -s_srs EPSG:4326 -t_srs EPSG:4326
 //        String cmd = "shp2pgsql -s 4326 -d " + shp.getAbsolutePath() + " public."+satname+"fire | psql -h localhost -d postgres -U postgres";
         String cmd = "ogr2ogr -skipfailures -overwrite -f \"PostgreSQL\" PG:\"host=localhost user=postgres dbname=postgres password=postgres\"" +
-                " "+ shp.getAbsolutePath()+" -nln public."+satname+"fire";
+                " " + shp.getAbsolutePath() + " -nln public." + satname + "fire";
         ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", cmd);
         try {
             Process p = pb.start();
@@ -121,19 +134,20 @@ public class DataCreator {
     }
 
     public static void downloadAndUpdate(String satname, int days) {
-        if (isUpToDate(satname,days))
+        if (isUpToDate(satname, days)) {
+            System.out.println("Updated, satname: " + satname + "; days: " + days);
             return;
+        } else
+            System.out.println("Not updated, satname: " + satname + "; days: " + days);
 
-        String zipfile = satname+".zip";
-        String folder = satname;
+        String zipfile = satname + ".zip";
         String url;
         String shapefile;
         if (satname == "modis") {
-            if (days ==1) {
+            if (days == 1) {
                 url = "https://firms.modaps.eosdis.nasa.gov/active_fire/c6/shapes/zips/MODIS_C6_Global_24h.zip";
                 shapefile = "MODIS_C6_Global_24h.shp";
-            }
-            else {
+            } else {
                 if (days == 2) {
                     url = "https://firms.modaps.eosdis.nasa.gov/active_fire/c6/shapes/zips/MODIS_C6_Global_48h.zip";
                     shapefile = "MODIS_C6_Global_48h.shp";
@@ -142,58 +156,164 @@ public class DataCreator {
                     shapefile = "MODIS_C6_Global_7d.shp";
                 }
             }
-        }
-        else {
+        } else {
             if (days == 1) {
                 url = "https://firms.modaps.eosdis.nasa.gov/active_fire/viirs/shapes/zips/VNP14IMGTDL_NRT_Global_24h.zip";
                 shapefile = "VNP14IMGTDL_NRT_Global_24h.shp";
-            }
-            else {
+            } else {
                 if (days == 2) {
                     url = "https://firms.modaps.eosdis.nasa.gov/active_fire/viirs/shapes/zips/VNP14IMGTDL_NRT_Global_48h.zip";
                     shapefile = "VNP14IMGTDL_NRT_Global_48h.shp";
-                }
-                else{
+                } else {
                     url = "https://firms.modaps.eosdis.nasa.gov/active_fire/viirs/shapes/zips/VNP14IMGTDL_NRT_Global_7d.zip";
                     shapefile = "VNP14IMGTDL_NRT_Global_7d.shp";
                 }
             }
         }
         DataCreator.downloadFileFromURL(url, zipfile);
-        DataCreator.unZipIt(zipfile, folder);
-
-        File shp = new File(folder, shapefile);
-        DataCreator.shpToDb(shp,satname);
+        System.out.println("Downloaded");
+        DataCreator.unZipIt(zipfile, satname);
+        System.out.println("Unziped");
+        File shp = new File(satname, shapefile);
+        DataCreator.shpToDb(shp, satname);
+        System.out.println("Pushed to DB");
         (new File(zipfile)).delete();
         try {
             FileUtils.deleteDirectory(shp.getParentFile());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
-    public static boolean isUpToDate(String satname,int days){
-
+    public static boolean isUpToDate(String satname, int days) {
         Connection c = null;
         Statement stmt = null;
+        boolean result = false;
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
                     .getConnection("jdbc:postgresql://localhost:5432/postgres",
                             "postgres", "postgres");
             c.setAutoCommit(false);
+
             stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "select exists(select 1 from "+satname+"fire where current_date = acq_date+"+days+");" );
-            while ( rs.next() ) {
-                return rs.getBoolean(1);
-            }
+            ResultSet rs = stmt.executeQuery("select exists(select 1 from " + satname + "fire where current_date <= acq_date+" + days + ");");
+            rs.next();
+            result = rs.getBoolean(1);
+
             rs.close();
             stmt.close();
             c.close();
-        } catch ( Exception e ) {
-            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return result;
         }
-
-        return false;
     }
+
+    public static RenderableLayer getPointsLayer(String satname) {
+        RenderableLayer result = new RenderableLayer();
+        result.setName("points");
+
+        PointPlacemarkAttributes ppattributes = new PointPlacemarkAttributes();
+        ppattributes.setUsePointAsDefaultImage(true);
+        ppattributes.setLineMaterial(new Material(Color.red));
+        ppattributes.setScale(4D);
+        try {
+            String dburl = "jdbc:postgresql://localhost/postgres";
+            Connection conn = DriverManager.getConnection(dburl, "postgres", "postgres");
+            Statement stat = conn.createStatement();
+//            ResultSet rs = stat.executeQuery("select st_x(s),st_y(s) from\n" +
+//                    "(SELECT  distinct ST_SnapToGrid(wkb_geometry,0.1) as s from "+satname+"fire) x");
+//            ResultSet rs = stat.executeQuery("select st_x(ST_Centroid(gc)),st_y(ST_Centroid(gc)), ST_NumGeometries(gc) " +
+//                    "from (select unnest(ST_ClusterWithin(wkb_geometry, 1)) gc from "+satname+"fire) f");
+
+            ResultSet rs = stat.executeQuery("select ST_ConcaveHull(unnest(ST_ClusterWithin(wkb_geometry, 1)),0.9) gc from " + satname + "fire");
+
+            while (rs.next()) {
+                Geometry geom = ((PGgeometry) rs.getObject(1)).getGeometry();
+                String s = geom.getTypeString();
+                if (s == "POLYGON") {
+                    ArrayList<LatLon> pos = new ArrayList<LatLon>();
+                    for (int i = 0; i < geom.numPoints(); i++) {
+                        pos.add(new LatLon(Angle.fromDegrees(geom.getPoint(i).y), Angle.fromDegrees(geom.getPoint(i).x)));
+                    }
+                    SurfacePolygon sp = new SurfacePolygon(pos);
+                    BasicShapeAttributes attributes = new BasicShapeAttributes();
+                    attributes.setInteriorMaterial(new Material(new Color(255, 0, 0)));
+                    attributes.setOutlineMaterial(new Material(new Color(255, 0, 0)));
+                    attributes.setInteriorOpacity(0.5);
+                    attributes.setOutlineOpacity(0.5);
+                    attributes.setOutlineWidth(4);
+                    sp.setAttributes(attributes);
+                    result.addRenderable(sp);
+                }
+                if (s == "POINT") {
+                    PointPlacemark p = new PointPlacemark(Position.fromDegrees(
+                            ((Point) geom).y, ((Point) geom).x
+                    ));
+                    p.setAttributes(ppattributes);
+                    result.addRenderable(p);
+                }
+                if (s == "LINESTRING") {
+                    for (int i = 0; i < geom.numPoints(); i++) {
+                        PointPlacemark p = new PointPlacemark(Position.fromDegrees(
+                                geom.getPoint(i).y, geom.getPoint(i).x
+                        ));
+                        p.setAttributes(ppattributes);
+                        result.addRenderable(p);
+                    }
+                }
+            }
+            stat.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static RenderableLayer getFireCountries(String satname) {
+        RenderableLayer result = new RenderableLayer();
+        result.setName("countries");
+        try {
+            String dburl = "jdbc:postgresql://localhost/postgres";
+            Connection conn = DriverManager.getConnection(dburl, "postgres", "246345");
+            Statement stat = conn.createStatement();
+
+            ResultSet rs = stat.executeQuery("select c.wkb_geometry, count(distinct m.wkb_geometry), avg(c.area) " +
+                    "from countries c left join " + satname + "fire m\n" +
+                    "on st_contains(c.wkb_geometry, m.wkb_geometry)\n" +
+                    "group by c.wkb_geometry\n" +
+                    "having count(distinct m.wkb_geometry) > 0\n");
+
+            while (rs.next()) {
+                Polygon p = (Polygon) ((PGgeometry) rs.getObject(1)).getGeometry();
+                ArrayList<LatLon> pos = new ArrayList<LatLon>();
+                for (int i = 0; i < p.numPoints(); i++) {
+                    pos.add(new LatLon(Angle.fromDegrees((p.getPoint(i).y > 180) ? 180 : p.getPoint(i).y),
+                            Angle.fromDegrees((p.getPoint(i).x > 180) ? 180 : p.getPoint(i).x)));
+                }
+                SurfacePolygon sp = new SurfacePolygon(pos);
+
+                BasicShapeAttributes attributes = new BasicShapeAttributes();
+                double intencity = 15D * rs.getDouble(2) / rs.getDouble(3) + 0.1;
+                if (intencity > 1)
+                    intencity = 1;
+
+                attributes.setInteriorMaterial(new Material(new Color(255, 0, 0)));
+                attributes.setInteriorOpacity(intencity);
+                attributes.setOutlineMaterial(new Material(new Color(255, 0, 0)));
+                sp.setAttributes(attributes);
+                result.addRenderable(sp);
+            }
+            stat.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
